8e7921be12543467867cae2d7008d97a551bc488
Billy Cheung <cheung.billywk@hutchisonports.com>
2023-09-25 17:05:39 +0800

    add react router

--- frontend/src/component/NavigationBar.tsx
+++ frontend/src/component/NavigationBar.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import { Dashboard, People, Settings } from '@mui/icons-material';
-import TableView from '../view/TableView';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, ListSubheader } from '@mui/material';
+import { ArrowBack, Dashboard } from '@mui/icons-material';
+
+import { SetStateAction, useState } from 'react';
 
 export const NavigationBar = () => {
+  const [selectedItem, setSelectedItem] = useState(0); // Track the selected list item
+
+  const handleItemClick = (index: SetStateAction<number>) => {
+    setSelectedItem(index);
+  };
+
   return (
     <Drawer variant="permanent">
+      <ListSubheader component="div" id="nested-list-subheader">
+          DashBoard
+      </ListSubheader>
       <List>
-        <ListItem
-        // component={<TableView/>}
-        >
-
-
+        <ListItem button onClick={() => handleItemClick(0)} selected={selectedItem === 0}>
           <ListItemIcon>
-            <Dashboard />
+            <ArrowBack />
           </ListItemIcon>
-          
-          <ListItemText primary="Dashboard" />
-
-
-        </ListItem>
+          </ListItem>
       </List>
     </Drawer>
   );
 };
-
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,14 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { DashboardView } from './view/DashboardView';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    loader: App,
+    children: [
+      {
+        path: "/Dashboard",
+        element: <DashboardView />,
+        // loader: dashboardLoader,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
